569186ab2f84c9cced52c68f4ca0da0164eb3ff2
dlang4 <dlang4@vcd-ch204-04.eastern.ewu.edu>
2016-11-08 14:33:04 -0800

    version 0.5
    start!

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 // YOUR SCRIPTS
+//grid view
+$('#grid').click(function(){
+    //remove list class ONLY
+    $('.cd-gallery').removeClass('list');
+    //add grid class ONLY
+    $('.cd-gallery').addClass('grid');
+});
+//grid view
+$('#list').click(function(){
+    //remove list class ONLY
+    $('.cd-gallery').removeClass('grid');
+    //add grid class ONLY
+    $('.cd-gallery').addClass('list');
+});
